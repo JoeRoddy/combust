f655ecbb7e9228014f2eaa4876b3aad1a562eac2
Joe <mrjoeroddy@gmail.com>
2017-11-03 15:46:02 -0400

    add example boilerplate

---
+++ service/MessageService.js
@@ -0,0 +1,63 @@
+import firebase from "firebase";
+
+class MessageService {
+   sendMessage(convoId, message) {
+    if (!convoId || !message) {
+      return;
+    }
+    message.createdAt = new Date().getTime();
+    let db = firebase.database();
+    let msgRef = db.ref("messages").push();
+    let msgId = msgRef.key;
+    msgRef.set(message);
+    db.ref("conversations/" + convoId + "/messages/" + msgId).set(true);
+  }
+
+   listenForNewMessages(convoId, callback) {
+    if (!convoId) {
+      return;
+    }
+    let db = firebase.database();
+    db
+      .ref("conversations/" + convoId + "/messages")
+      .on("child_added", snapshot => {
+        let msgId = snapshot.key;
+        db
+          .ref("messages/")
+          .child(msgId)
+          .once("value", (err, msg) => {
+            callback(msg);
+          });
+      });
+  }
+
+   toggleUserTyping(convoId, userId, isTyping) {
+    if (!convoId || !userId) {
+      return;
+    }
+    let ref = firebase
+      .database()
+      .ref("conversations/" + convoId + "/participants/" + userId);
+    ref.set(isTyping);
+  }
+
+   listenToConversation(convoId, callback) {
+    if (!convoId) {
+      return;
+    }
+    let db = firebase.database();
+    db.ref("conversations/" + convoId).on("value", snapshot => {
+      let msgId = snapshot.key;
+      let convo = snapshot.val();
+      convo.id = convoId;
+      callback(convo);
+    });
+  }
+
+   createConversation(req, callback) {
+    //TODO:
+    }
+}
+
+export let userService = new UserService();
+
