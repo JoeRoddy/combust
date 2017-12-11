68e7b6daa0b17136f369b4b281f181bb4cb778cf
Joe <mrjoeroddy@gmail.com>
2017-12-11 11:03:48 -0500

    sketch of install functionality

--- command_handlers/install.js
+++ command_handlers/install.js
@@ -1,15 +1,23 @@
 let shell = require("shelljs");
-//eventually: mobile - web - desktop
+
 module.exports = moduleName => {
   if (!moduleName)
-    return console.log(
-      "Err: Must specify a moduleNameule: combust install moduleNameuleName\nView available moduleNameules here: http://www.example.com"
+    return console.error(
+      "Err: Must specify a moduleNameule: combust install moduleNameuleName\nView available modules here: http://www.example.com"
     );
   moduleName.toLowerCase();
   const firstCap = moduleName.charAt(0).toUpperCase() + moduleName.substring(1);
   console.info(`adding: ${moduleName}`);
-  console.info("BREH");
+
+  //download npm contents and unzip into temp directory
   shell.exec(
-    `mkdir ${moduleName} && touch ${moduleName}/${moduleName}.html && touch ${moduleName}/${firstCap}Service.js`
+    `mkdir -p temp && cd temp && npm pack combust-${moduleName} | awk '{print "./"$1}' | xargs tar -xvzf $1`
   );
+
+  //extract files we need
+  shell.exec(`mkdir -p stores && mkdir -p service`);
+  shell.exec(`mv temp/package/Store.js ./stores/${firstCap}Store.js`);
+  shell.exec(`mv temp/package/Service.js ./service/${firstCap}Service.js`);
+
+  shell.exec(`rm -rf temp`);
 };
