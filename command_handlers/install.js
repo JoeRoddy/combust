b56c67697be8f221210d900e8bd1a884c3a63e8c
Joe <mrjoeroddy@gmail.com>
2017-12-12 15:48:27 -0500

    fix file operations so that they don't overwrite each other

--- command_handlers/install.js
+++ command_handlers/install.js
@@ -1,7 +1,7 @@
 let shell = require("shelljs");
 const fs = require("fs");
 
-module.exports = moduleName => {
+function install(moduleName) {
   if (!moduleName)
     return console.error(
       "Err: Must specify a module: combust install moduleName\nView available modules here: http://www.example.com"
@@ -14,81 +14,79 @@ module.exports = moduleName => {
   const servicePath = `src/service/${firstCap}Service.js`;
   const componentsPath = `src/components/combust_examples/${moduleName}`;
   if (fs.existsSync(storePath) || fs.existsSync(servicePath)) {
-    // return console.info(moduleName + " already installed");
+    return console.info(moduleName + " already installed");
   }
 
+  const tempFolder = "temp" + firstCap;
   //download npm contents and unzip into temp directory
   const {
     stdout,
     stderr,
     code
   } = shell.exec(
-    `mkdir -p temp && cd temp && npm pack combust-${moduleName} | awk '{print "./"$1}' | xargs tar -xvzf $1`,
+    `mkdir -p ${tempFolder} && cd ${tempFolder} && npm pack combust-${moduleName} | awk '{print "./"$1}' | xargs tar -xvzf $1`,
     { silent: true }
   );
   stderr && console.error(stderr);
 
+  const instructions = JSON.parse(
+    fs.readFileSync(`${tempFolder}/package/combust.json`, "utf8")
+  );
+  downloadDependencies(instructions.dependencies);
+
+  console.log("about to extract files for " + storePath);
   //extract files we need and move to src
   shell.exec(`mkdir -p src && mkdir -p src/stores && mkdir -p src/service`);
-  shell.exec(`mv temp/package/Store.js ${storePath}`);
-  shell.exec(`mv temp/package/Service.js ${servicePath}`);
+  shell.exec(`mv ${tempFolder}/package/Store.js ${storePath}`);
+  shell.exec(`mv ${tempFolder}/package/Service.js ${servicePath}`);
   shell.exec(
-    `mkdir -p ${componentsPath} && mv temp/package/components/* ${componentsPath}`
+    `mkdir -p ${componentsPath} && mv ${tempFolder}/package/components/* ${componentsPath}`
   );
 
-  const instructions = JSON.parse(
-    fs.readFileSync("temp/package/combust.json", "utf8")
-  );
+  //execute installation instructions
   const installInstructions = instructions ? instructions.installation : null;
-
   executeInstallInstructions(installInstructions);
-  updateCombustConfig(moduleName);
 
-  shell.exec(`rm -rf temp`);
-};
+  updateCombustConfig(moduleName);
+  shell.exec(`rm -rf ${tempFolder}`);
+}
+
+function downloadDependencies(dependencies) {
+  for (let dependency in dependencies) {
+    install(dependency);
+  }
+}
 
 function executeInstallInstructions(installInstructions) {
-  // console.log("exec install called w/:", installInstructions);
+  console.log("exec install called w/:", installInstructions);
   installInstructions &&
     Object.keys(installInstructions).forEach(path => {
       const filePath = "src/" + path;
-      fs.readFile(filePath, "utf8", (err, file) => {
-        if (err) {
-          return console.error(err);
-        }
-        const fileInstructions = installInstructions[path];
-        Object.keys(fileInstructions).forEach(operation => {
-          const linesToInsert = fileInstructions[operation];
-          file = operationsMap[operation](file, linesToInsert);
-        });
+      let file = fs.readFileSync(filePath);
 
-        fs.writeFile(filePath, file, "utf-8", err => {
-          if (err) throw err;
-          console.log("added code to " + filePath);
-        });
+      const fileInstructions = installInstructions[path];
+      Object.keys(fileInstructions).forEach(operation => {
+        const linesToInsert = fileInstructions[operation];
+        file = operationsMap[operation](file, linesToInsert);
       });
+
+      fs.writeFileSync(filePath, file);
     });
 }
 
 function updateCombustConfig(moduleName) {
   const filePath = "src/.combust/init.js";
-  fs.readFile(filePath, "utf8", (err, file) => {
-    if (err) {
-      return console.error(err);
-    }
-    const firstCap =
-      moduleName.charAt(0).toUpperCase() + moduleName.substring(1);
-
-    file = insertImports(file, [
-      `import ${moduleName}Store from "../stores/${firstCap}Store"`
-    ]);
-    file = insertAfter(file, [`\t${moduleName}Store,`], "stores = {\n");
-
-    fs.writeFile(filePath, file, "utf-8", err => {
-      if (err) throw err;
-      console.log("added code to " + filePath);
-    });
+  let file = fs.readFileSync(filePath);
+  const firstCap = moduleName.charAt(0).toUpperCase() + moduleName.substring(1);
+  file = insertImports(file, [
+    `import ${moduleName}Store from "../stores/${firstCap}Store"`
+  ]);
+  file = insertAfter(file, {
+    pattern: "stores = {\n",
+    code: [`\t${moduleName}Store,`]
   });
+
+  fs.writeFileSync(filePath, file);
 }
 
 const operationsMap = {
@@ -96,7 +94,8 @@ const operationsMap = {
   renderStart: insertAtRenderStart,
   renderEnd: insertAtRenderEnd,
   after: insertAfter,
-  before: insertBefore
+  before: insertBefore,
+  replace: replaceCode
 };
 
 function insertImports(file, code) {
@@ -106,32 +105,36 @@ function insertImports(file, code) {
   return file;
 }
 
-function insertAfter(file, code, pattern) {
-  const codeToAdd = getCodeStrFromArr(code);
-  const index = file.indexOf(pattern) + pattern.length;
-  if (index < pattern.length) return file;
-  return insertCodeAtIndex(file, codeToAdd, index);
+function replaceCode(file, patternAndCode) {
+  const { pattern, code } = patternAndCode;
+  return file.replace(pattern, getCodeStrFromArr(code));
 }
 
-function insertBefore(file, code, pattern) {
-  const codeToAdd = getCodeStrFromArr(code);
+function insertAfter(file, patternAndCode) {
+  const { pattern, code } = patternAndCode;
+  const index = file.indexOf(pattern) + pattern.length;
+  if (index < pattern.length) return file;
+  return insertCodeAtIndex(file, code, index);
+}
+
+function insertBefore(file, patternAndCode) {
+  const { pattern, code } = patternAndCode;
   const index = file.indexOf(pattern);
-  return insertCodeAtIndex(file, codeToAdd, index);
+  return insertCodeAtIndex(file, code, index);
 }
 
 function insertAtRenderEnd(file, code) {
-  const codeToAdd = getCodeStrFromArr(code);
   const index = file.lastIndexOf("</");
-  return insertCodeAtIndex(file, codeToAdd, index);
+  return insertCodeAtIndex(file, code, index);
 }
 
 function insertAtRenderStart(file, code) {
-  const codeToAdd = getCodeStrFromArr(code);
   const index = file.indexOf("<");
-  return insertCodeAtIndex(file, codeToAdd, index);
+  return insertCodeAtIndex(file, code, index);
 }
 
 function insertCodeAtIndex(file, code, index) {
+  code = getCodeStrFromArr(code);
   if (index < 0) return file;
   return [file.slice(0, index), code, file.slice(index)].join("");
 }
@@ -144,3 +147,5 @@ function getCodeStrFromArr(codeArray) {
     });
   return codeStr;
 }
+
+module.exports = install;
